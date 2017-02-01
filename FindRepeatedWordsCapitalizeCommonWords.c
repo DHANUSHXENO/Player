5a84c230100ffe463757fda6b3d38e170cdbee48
DHANUSHXENO <dhanushxenocrate@gmail.com>
2017-02-01 18:03:40 +0530

    Add files via upload

---
+++ FindRepeatedWordsCapitalizeCommonWords.c
@@ -0,0 +1,22 @@
+#include"stdio.h"
+main()
+{
+    char ch[50];int i,j,flag=0;
+    printf("Enter a word");
+    gets(ch);
+    for(i=0;i<strlen(ch);i++)
+    {
+        for(j=0;j<strlen(ch);j++)
+        {
+            if(ch[i]==ch[j] && i!=j)
+            {
+                flag=1;
+            }
+        }
+        if(flag==1)
+            printf("%c",ch[i]-32);
+        else
+            printf("%c",ch[i]);
+        flag=0;
+    }
+}
