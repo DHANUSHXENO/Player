3a928704c30c56e52125508ceaccba737046752d
DHANUSHXENO <dhanushxenocrate@gmail.com>
2017-02-01 17:20:17 +0530

    Add files via upload

---
+++ ImplementStringLengthMethod.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    char ch[10];int i,count=0;
+    printf("Enter a string :");
+    gets(ch);
+    for(i=0;ch[i]!='\0';i++)
+    {
+        ++count;
+    }
+    printf("The length of the string is %d",count);
+}
