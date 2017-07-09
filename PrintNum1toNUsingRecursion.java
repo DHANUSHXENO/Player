6c33a7c1b3d73b65be363ebc1e0f5b18be698241
DHANUSH <dhanushxenocrate@gmail.com>
2017-07-09 23:14:43 +0530

    Add files via upload

---
+++ PrintNum1toNUsingRecursion.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class PrintNum1toNUsingRecursion {
+	static int i=0;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		System.out.println(print(n));
+		sc.close();
+	}
+	static String print(int n) {
+		if(i==n)
+			return "";
+		else
+			return (++i)+print(n);
+	}
+
+}
