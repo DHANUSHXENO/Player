1c0150767a7c61a2ac22464ec8362f1f06d8af1e
DHANUSH <dhanushxenocrate@gmail.com>
2017-07-13 18:13:48 +0530

    Add files via upload

---
+++ ConcatWordsWithoutUsingStringFunc.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class ConcatWordsWithoutUsingStringFunc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter word 1");
+		String str1=sc.next();
+		System.out.println("Enter word 2");
+		String str2=sc.next();
+		System.out.println(str1+" "+str2);
+	}
+}
