3176196a0502e4f3b715a52f8f40e0647ad0f276
DHANUSH <dhanushxenocrate@gmail.com>
2017-07-17 09:25:08 +0530

    Add files via upload

---
+++ SumOfOddNumWithInputRange.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class SumOfOddNumWithInputRange {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int sum=0;
+		for(int i=1;i<=n;i+=2) {
+			sum=sum+i;
+			
+		}
+		System.out.println(sum);
+		sc.close();
+	}
+}
